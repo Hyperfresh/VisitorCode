b7716879ca61cca4ff8f3faa3b91afa784d89f9e
Hy~ <31476608+Hyperfresh8080@users.noreply.github.com>
2020-03-23 22:16:41 +1030

    A lot of progress. Phew.

--- main.py
+++ main.py
@@ -4,9 +4,29 @@
 # Planning and programming a visitor register
 # Concordia College | SACE 740975X
 
-# Code will eventually appear™
-logo = """
-       ! `c `_                                                                                           
+# Library importing
+import keyboard
+from termcolor import colored
+import time
+import datetime
+import getpass
+
+# Variable declaration
+name = ""
+NumberToCheck = 0
+phone = 0
+pin = 0
+pincheck = 0
+sign = False
+reset = False
+CurrentTime = ""
+
+def UpdateTime():
+    global CurrentTime
+    CurrentTime = (time.strftime("%d %b %Y %H:%M:%S", time.localtime()))
+
+# Big ASCII stuff
+logo = """       ! `c `_                                                                                           
     -Tig9g#$$U)x                                                                                         
   !Yd#@@QDEEB@@QXr-                                                                                      
   xIXmoTT*:v}cIsyd-      `)IM5k^  `voMqz*  'hUv  :hh.  roqMj\` `*zMMI)` `hhhhIV*  Yhhhj}<  -hh,   ^33^   
@@ -20,6 +40,97 @@ r6   )xrx>uxvYL   .g    vR-   `_'$~    r0`!#     .#      Q'     -$:    . `#
 3]   )is}rVLkhY    8'   Q*      vK      R~!#     .#      Qliii* }y  .xxy:`#Liii<                         
 d)   ';6!qKyTc"    R_   }6`    _-8:    =g`!#     .#      Q'     ,g-    K*`#                              
 ,s}r=:!!"=:!:!::<xVT     _l}L}u: `xuYLu?  _WLLLL<`HLLLLr o}iiii_ 'YTii}x``aLiiii.                        
-  `:;*^^)uy}*^**=-                                                                                                                                                                                                                                                                                                                                                                
-"""
-print(logo)
+  `:;*^^)uy}*^**=-"""
+options = """ _________                                                           _________    _______
+|         |  \      /  .        .                                   |   ___   |  |         .
+|   /|    |   \    /       ___     _|_   ___    __                  |   ___|  |  |______       ___   ___     ___        _|_
+|   _|_   |    \  /    |  |___  |   |   |   |  |                    |   ___|  |         |  |  |   | |   |   |   | |   |  |
+|_________|     \/     |   ___| |   |   |___|  |                    |_________|  _______|  |  |___| |   |   |___| |___|  |
+ _________    ______                                                 _________    _____        ___/                   ___  _____
+|   __    |  |                                                      |         |  |                                   |   |   |   |\   |
+|    _\   |  |        ___   ___  _|_  __  ___    __ _|_   ___   __  |   /_|   |  |___    ___   __  ___   ___  _|_    |___|   |   | \  |
+|   \__   |  |       |   | |   |  |  |   |   |  |    |   |   | |    |     |   |  |      |   | |   |   | |   |  |     |       |   |  \ |
+|_________|  |______ |___| |   |  |  |   |___|\ |__  |   |___| |    |_________|  |      |___| |   |___| |___|  |     |     __|__ |   \|
+                                                                                                   ___/"""
+# String checks if it can be converted to a number.
+def NumCheck(Prompt):
+    global NumberToCheck
+    NumberToCheck = str((input(Prompt + " > ")))
+    try:
+        NumberToCheck = int(NumberToCheck)
+    except:
+        print(colored("Sorry, please try that again.","red"))
+        NumCheck(Prompt)
+
+# PIN
+
+# Visitor module
+def Visitor():
+    global NumberToCheck
+    global name
+    global phone
+    global pin
+    global pincheck
+    global sign
+    global CurrentTime
+    name = str(input("Please enter your name. > "))
+    NumCheck("Enter your phone number.")
+    phone = NumberToCheck
+    if reset == True:
+        # I could put in some code to check if the entry exists...
+        print("Your entry " + str(name) + " with phone number 0" + str(phone) + " will be reset. Confirm?")
+    else:
+        print("An entry for " + str(name) + " with phone number 0" + str(phone) + " will be created. Confirm?")
+    while True:  # making a loop
+        try:  # used try so that if user pressed other than the given key error will not be shown
+            if keyboard.is_pressed('n'):
+                print("Okay, cancelling.")
+                time.sleep(3)
+                UI()
+            if keyboard.is_pressed('y'):
+                break
+        except:
+            break  # if user pressed a key other than the given key the loop will break
+    print(colored("You are about to set a PIN. It must be numeric and cannot start with 0.","yellow"))
+    def pinchecker():
+        global pin
+        global pincheck
+        try:
+            pin = int(getpass.getpass("Please enter a PIN. > "))
+            pincheck = int(getpass.getpass("Enter it again      > "))
+            if pincheck != pin:
+                print(colored("Sorry, the PINs you entered didn't match.","red"))
+                pinchecker()
+        except:
+            print(colored("Sorry, please try that again.","red"))
+            pinchecker()
+    pinchecker()
+    if reset == False:
+        sign = True
+    # File writing.
+    UpdateTime()
+    file2 = open('database.txt', 'a')
+    file2.write(str(name) + ', ')
+    file2.write(str(phone) + ', ' + str(pin) + ', ' + str(sign) + ', ' + str(CurrentTime) + " \n")
+    file2.close()
+    print("Thank you, " + str(name) + ".")
+    if reset == True:
+        print("Your account has been successfully reset.\nPlease sign in by pressing [2] on the next screen.")
+    else:
+        print("You have been signed in. Please remember your PIN.")
+    time.sleep(5)
+    UI()
+    
+# User interface module
+def UI():
+    print(logo)
+    print(options)
+    while True:
+        try:
+            if keyboard.is_pressed('1'):
+                Visitor()
+        except:
+            break
+
+# And this is the thing that calls it. Pretty boring I know.
+UI()
